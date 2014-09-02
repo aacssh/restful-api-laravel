4707f3c4820f290690615efd643f436539438eeb
Aashish Ghale <aashish.ghale@gmail.com>
2014-09-03 00:40:21 +0545

    Removed some routes, added some routes on September 2, 2014

--- app/routes.php
+++ app/routes.php
@@ -1,8 +1,11 @@
 <?php
 Route::group(['prefix' => 'api/v1'], function(){
-    Route::post('login', 'UsersController@store');
-    Route::post('logout', 'UsersController@logout');
-    Route::post('register', 'UsersController@register');
-    Route::resource('users', 'UsersController');
-    Route::resource('barbers', 'BarbersController');
+
+    Route::post('login', ['as' => 'api.v1.users.login', 'uses' => 'UsersController@store']);
+    Route::get('logout', ['as' => 'api.v1.users.logout', 'uses' => 'UsersController@destroy']);
+    Route::post('register', ['as' => 'api.v1.users.register', 'uses' => 'RegistersController@store']);
+    Route::resource('barber', 'BarbersController', ['except' => ['store', 'create', 'edit']]);
+    Route::resource('client', 'ClientsController', ['except' => ['store', 'create', 'edit']]);
+    Route::resource('barber.appointments', 'AppointmentsController', ['except' => ['store', 'create', 'edit', 'update']]);
+    Route::resource('client.appointments', 'AppointmentsController', ['except' => ['create', 'edit', 'update']]);
 });
